8a0a345ead6e097d7c35609891819b891a7cd886
katelynarmstrong <katelynarmstrong6@gmail.com>
2022-01-01 13:45:56 -0600

    prompts created

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var password = ''
+  var promptCriteria = window.prompt('How many characters would you like, between 8-128?');
+  var promptCriteria = window.prompt('Would you like to include special characters?');
+  var promptCriteria = window.prompt('Would you like to include lower case letters?');
+  var promptCriteria = window.prompt('Would you like to include upper case letters?');
+
+
+  //charAt()
+  //while loops
+  //for loops
+
+  return password
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
