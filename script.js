5897db6a6d103b430d507ae34d847325ced90e91
katelynarmstrong <katelynarmstrong6@gmail.com>
2022-01-04 18:17:23 -0600

    working generator

--- script.js
+++ script.js
@@ -1,35 +1,74 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// 
 function generatePassword() {
   var password = ''
-  var specialCharacters = ["!@#$%^&*"]
-  var lowerCase = ["abcdefghijklmnopqrstuvwxyz"]
-  var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
-  var numericValues = ["0123456789"]
+  var allCharacters = ''
+  var specialCharacters = "!@#$%^&*"
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var numericValues = "0123456789"
 
+  // 
   var passwordLength = window.prompt('How many characters would you like, between 8-128?');
-    if (passwordLength < 8) {
-      window.alert("Please select a password length between 8-128")
-    }
-    if (passwordLength > 128) {
-      window.alert("Please select a password length between 8-128")
-    }
-    console.log(passwordLength)
+  while (passwordLength < 8 || passwordLength > 128) {
+    alert('Your password must be a number between 8 and 128')
+    passwordLength = window.prompt('How many characters would you like, between 8-128?');
+  }
+  console.log(passwordLength)
 
-
-  confirm('Would you like to include special characters?');
+// 
+  var wantsSpecial = confirm('Click ok to confirm the use of special characters.');
   console.log(specialCharacters)
-  
-  confirm('Would you like to include lower case letters?');
+
+  var wantsLower = confirm('Click ok to confirm the use of lower case letters.');
   console.log(lowerCase)
-  
-  confirm('Would you like to include upper case letters?');
+
+  var wantsUpper = confirm('Click ok to confirm the use of upper case letters.');
   console.log(upperCase)
 
-  confirm('Would you like to include numbers?');
+  var wantsNumbers = confirm('Click ok to confirm the use of numbers.');
   console.log(numericValues)
 
+  // 
+  while (!wantsSpecial && !wantsLower && !wantsUpper && !wantsNumbers) {
+    alert('You must choose at least one character type for your password')
+    wantsSpecial = confirm('Click ok to confirm the use of special characters.');
+    console.log(specialCharacters)
+
+    wantsLower = confirm('Click ok to confirm the use of lower case letters.');
+    console.log(lowerCase)
+
+    wantsUpper = confirm('Click ok to confirm the use of upper case letters.');
+    console.log(upperCase)
+
+    wantsNumbers = confirm('Click ok to confirm the use of numbers.');
+    console.log(numericValues)
+  }
+// 
+if(wantsSpecial) {
+  allCharacters += specialCharacters
+}
+
+if (wantsLower) {
+  allCharacters += lowerCase
+}
+
+if (wantsUpper) {
+  allCharacters += upperCase
+}
+
+if (wantsNumbers) {
+  allCharacters += numericValues
+}
+
+console.log(allCharacters);
+
+// 
+for (var i = 0; i < passwordLength; i++) {
+  password += allCharacters.charAt(Math.floor(Math.random() * allCharacters.length))
+}
 
   return password
 }
